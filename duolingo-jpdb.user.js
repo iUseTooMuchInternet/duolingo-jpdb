d7d1e47ee7d5eed617667ade25e15466e44c7dd1
no name is permanent <98455140+iUseTooMuchInternet@users.noreply.github.com>
2025-06-15 09:23:33 +0000

    insert css rules with a for each loop instead

--- duolingo-jpdb.user.js
+++ duolingo-jpdb.user.js
@@ -1,93 +1,57 @@
 let style1 = document.querySelector('[href="/static/be945648d268.css"]').sheet;
 
-style1.insertRule(
+/* List of rules */
+const rules = [
   `#grade-1, #grade-2, #grade-3, #grade-4, #grade-5 {
     color: #ffffff; 
     transition: all 250ms ease-out; 
     text-decoration: none;
   }`,
-  style1.cssRules.length
-);
-
-style1.insertRule(
   `#grade-1 {
     border-bottom: 5px solid #ea2b2b; 
     background-color: #ff4b4b;
   }`,
-  style1.cssRules.length
-);
-
-style1.insertRule(
   `#grade-2 {
     border-bottom: 5px solid #ea2b2b;
     background-color: #ff4b4b;
   }`,
-  style1.cssRules.length
-);
-style1.insertRule(
   `#grade-3 {
     border-bottom: 5px solid #ff9600;
     background-color: #ffb100;
   }`,
-  style1.cssRules.length
-);
-style1.insertRule(
   `#grade-4 {
     border-bottom: 5px solid #58a700;
     background-color: #58cc02;
   }`,
-  style1.cssRules.length
-);
-
-style1.insertRule(
   `#grade-5 {
     border-bottom: 5px solid #1899d6;
     background-color: #1cb0f6;
   }`,
-  style1.cssRules.length
-);
-
-style1.insertRule(
   `#grade-1:hover, #grade-2:hover, #grade-3:hover, #grade-4:hover, #grade-5:hover {
     box-shadow: none; 
     text-decoration: none;
   }`,
-  style1.cssRules.length
-);
-
-style1.insertRule(
   `#grade-1:hover {
     background-color: #f5a4a4;
   }`,
-  style1.cssRules.length
-);
-style1.insertRule(
   `#grade-2:hover {
     background-color: #f5a4a4;
   }`,
-  style1.cssRules.length
-);
-
-style1.insertRule(
   `#grade-3:hover {
     background-color: #ffc800; 
   }`,
-  style1.cssRules.length
-);
-
-style1.insertRule(
   `#grade-4:hover {
     background-color: #61e002; 
   }`,
-  style1.cssRules.length
-);
-
-style1.insertRule(
   `#grade-5:hover {
     background-color: #84d8ff; 
   }`,
-  style1.cssRules.length
-);
+];
+
+rules.forEach((rule) => {
+  style1.insertRule(rule, style1.cssRules.length);
+})
+
 console.log(style1.cssRules.length);
 
 let grade1;
